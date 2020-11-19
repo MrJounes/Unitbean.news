b98710f049d086575d389984432d2684ac5615a5
Игорь Дикань <i.dikan@qsoft.ru>
2020-11-19 13:37:44 +0400

    Инициализация проекта

---
+++ Unitbean.News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unitbean.News
+//
+//  Created by Игорь Дикань on 19.11.2020.
+//  Copyright © 2020 Игорь Дикань. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
